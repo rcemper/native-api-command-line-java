d4ac72c616d0154f3cce42c5daa4cb420ac24fb8
r-cemper <146277387+r-cemper@users.noreply.github.com>
2023-11-17 22:04:24 +0100

    JDBC versions change to fast

--- src/pre.sh
+++ src/pre.sh
@@ -2,4 +2,4 @@
 
 echo "Building..."
 cd /opt/irisapp
-javac -classpath /usr/irissys/dev/java/lib/1.8/intersystems-jdbc-3.7.1.jar -d . src/rcc.java
+javac -classpath /usr/irissys/dev/java/lib/1.8/intersystems-jdbc-3.*.jar -d . src/rcc.java
